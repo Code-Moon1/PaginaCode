2dfe25d68f8f487cdc9a734170caf2a6ef667f7f
Victor <manuelher1619@gmail.com>
2023-04-29 11:01:01 -0600

    se hizo responsivo el formulario usuario

--- js/usuario.js
+++ js/usuario.js
@@ -1,18 +1,12 @@
 const formulario = document.getElementById('formulario');
 
+
 formulario.addEventListener('submit', function(event) {
   event.preventDefault();
   const usuario = formulario.usuario.value;
   const contrasena = formulario.contrasena.value;
   if (usuario === 'CodeMoon' && contrasena === 'Podcast1') {
-    Swal.fire({
-      title: "¡Bienvenidos!",
-      titlecolor:"#000000",
-    
-      confirmButtonClass: "custom-confirm-button-class",
-      confirmButtonColor: "#000000",
-        imageUrl: '/img/16.png',
-    });
+   
     // Acceso permitido
     setTimeout(() => {
     window.location.href = 'productos.html'; // Cambiar esto por la página a la que quieres redirigir
@@ -30,27 +24,50 @@ formulario.addEventListener('submit', function(event) {
     });
   }
 });
-/*if (codemoon) {
-  Swal.fire({
-    title: "¡Bienvenidxs!",
-    icon: "success",
-    confirmButtonClass: "custom-confirm-button-class",
-    confirmButtonColor: "#e23d82",
-      imageUrl: '/fotos/logo22.png',
-  });
-  
-  // Redireccion en caso exitoso
-  setTimeout(() => {
-    window.location.href = "/index.html";
-  }, 2000);
+// formulaio para unirlo con el back y que sea seguro
+/*// Obtener los elementos HTML
+const formulario = document.getElementById('formulario');
+const usuarioInput = document.getElementById('InputUsuario');
+const contrasenaInput = document.getElementById('InputContraseña');
 
-  //Mostrar mensaje de error si la validacion falla
-    alertError.innerHTML =
-      "El correo electrónico o la contraseña son incorrectos.";
-    alertError.style.display = "block";
-    setTimeout(() => {
-      alertError.style.display = "none";
-    }, 2000);
+// Agregar el evento de envío de formulario
+formulario.addEventListener('submit', function (event) {
+  // Evitar el envío del formulario
+  event.preventDefault();
+
+  // Obtener los valores del formulario
+  const usuario = usuarioInput.value;
+  const contrasena = contrasenaInput.value;
+
+  // Validar los campos del formulario
+  if (!usuario || !contrasena) {
+    alert('Por favor ingrese su nombre de usuario y contraseña.');
+    return;
   }
+
+  // Realizar la petición AJAX para autenticar al usuario
+  fetch('/api/login', {
+    method: 'POST',
+    body: JSON.stringify({
+      usuario: usuario,
+      contrasena: contrasena
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error en la autenticación');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Si la autenticación es exitosa, redirigir al usuario a la página principal
+    window.location.href = '/home';
+  })
+  .catch(error => {
+    alert(error.message);
+  });
 });
-}*/
+*/
